67517da66b6a28a3e5f25deb20b47d6dc2f3e71d
JD <43763092+jdw1023@users.noreply.github.com>
2023-04-02 03:26:07 -0400

    webpage demo for parser

--- src/scripts/utils/BlockVisitor.ts
+++ src/scripts/utils/BlockVisitor.ts
@@ -4,19 +4,46 @@ import BlockLangVisitor from '../../ANTLR4/generated/BlockLangVisitor';
 export default class BlockVisitor extends BlockLangVisitor<void> {
 	// https://github.com/Microsoft/TypeScript/issues/9722
 
+	output?: HTMLElement | null;
+	indent: number;
+
+	constructor(output?: HTMLElement | null) {
+		super();
+		this.indent = 0;
+		this.output = output;
+	}
+
 	visitProgram = (ctx: ParserRuleContext) => {
-		console.log('visitProgram', ctx);
+		this.indent = 0;
+		if (this.output) {
+			this.output.innerHTML = '';
+		}
+		// console.log('visitProgram', ctx);
+		return this.visitChildren(ctx);
 	}
 
 	visitStatement = (ctx: ParserRuleContext) => {
-		console.log('visitStatement', ctx);
+		if (this.output) {
+			this.output.innerHTML += `${'&nbsp;'.repeat(this.indent * 4)}visitStatement ${ctx.getText()}<br>`;
+		}
+		// console.log('visitStatement', ctx);
+		return this.visitChildren(ctx);
 	}
 
 	visitLoopStatement = (ctx: ParserRuleContext) => {
-		console.log('visitLoopStatement', ctx);
+		if (this.output) {
+			this.output.innerHTML += `${'&nbsp;'.repeat(this.indent * 4)}visitLoopStatement ${ctx.getText()}<br>`;
+		}
+
+		// console.log('visitLoopStatement', ctx);
+		return this.visitChildren(ctx);
 	}
 
 	visitMoveStatement = (ctx: ParserRuleContext) => {
-		console.log('visitMoveStatement', ctx);
+		if (this.output) {
+			this.output.innerHTML += `${'&nbsp;'.repeat(this.indent * 4)}visitMoveStatement ${ctx.getText()}<br>`;
+		}
+		// console.log('visitMoveStatement', ctx);
+		return this.visitChildren(ctx);
 	}
 }
