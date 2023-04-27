61267e9b20b7eddc7d6e9960ccca05da840aa56f
JD <43763092+jdw1023@users.noreply.github.com>
2023-04-27 03:04:40 -0400

    add editor color and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 	plugins: [],
 	server: { host: '0.0.0.0', port: 8000 },
 	clearScreen: false,
+	base:"/group-project-build-blocks/",
 	build: {
     rollupOptions: {
       input: {
