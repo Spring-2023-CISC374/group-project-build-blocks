3ab8d76b5d8c4d19376ef421e2525a779b53cfe7
ydnamjs <ydnamjs@udel.edu>
2023-04-28 11:59:48 -0400

    altered image load statements to hopefully work with deployment

--- src/scenes/LevelSelectScene.ts
+++ src/scenes/LevelSelectScene.ts
@@ -66,13 +66,13 @@ export default class LevelSelectScene extends Phaser.Scene {
         this.load.image('cranePickupBox', 'assets/CranePickupBox.png');
         this.load.spritesheet('visibilityButton', 'assets/VisibilityButtons.png', {frameWidth: 48, frameHeight: 32});
 		
-		this.load.image('instruction-start', '../public/assets/StartInstruction.png');
-		this.load.image('instruction-loop', '../public/assets/LoopInstruction.png');
-		this.load.image('instruction-endloop', '../public/assets/EndloopInstruction.png');
-		this.load.image('instruction-up', '../public/assets/UpInstruction.png');
-		this.load.image('instruction-down', '../public/assets/DownInstruction.png');
-		this.load.image('instruction-left', '../public/assets/LeftInstruction.png');
-		this.load.image('instruction-right', '../public/assets/RightInstruction.png');
+		this.load.image('instruction-start', 'assets/StartInstruction.png');
+		this.load.image('instruction-loop', 'assets/LoopInstruction.png');
+		this.load.image('instruction-endloop', 'assets/EndloopInstruction.png');
+		this.load.image('instruction-up', 'assets/UpInstruction.png');
+		this.load.image('instruction-down', 'assets/DownInstruction.png');
+		this.load.image('instruction-left', 'assets/LeftInstruction.png');
+		this.load.image('instruction-right', 'assets/RightInstruction.png');
 	}
 
 	create() {
