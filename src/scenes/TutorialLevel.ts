68f7ea6dcdadc5a7ed898505bca57ed7f8d5fbbc
ydnamjs <ydnamjs@udel.edu>
2023-05-09 12:26:45 -0400

    Added new blocks for tutorial and way to keep track of them in tutorial level class

--- src/scenes/TutorialLevel.ts
+++ src/scenes/TutorialLevel.ts
@@ -1,3 +1,5 @@
+import Instruction from '../objects/Instruction';
+import { InstructionType } from '../types/InstructionType';
 import { GridData } from './../interfaces/GridData';
 import Level from './Level';
 
@@ -31,16 +33,17 @@ export default class TutorialLevel extends Level {
 		number_blocks: [],
 	};
 
-	private static start_blocks = 1;
-	private static loop_blocks= 1;
-	private static endloop_blocks= 1;
-	private static left_blocks= 2;
-	private static right_blocks= 1;
-	private static down_blocks= 1;
-	private static up_blocks= 1;
-	private static grab_blocks= 1;
-	private static release_blocks= 1;
-	private static number_blocks= [2];
+    declare protected start_instruction?: Instruction;
+	protected loop_instruction?: Instruction;
+	protected grab_instruction?: Instruction;
+	protected release_instruction?: Instruction;
+	protected endloop_instruction?: Instruction;
+	protected up_instruction?: Instruction;
+	protected down_instruction?: Instruction;
+	protected right_instruction?: Instruction;
+	protected left1_instruction?: Instruction;
+	protected left2_instruction?: Instruction;
+	protected number_instruction?: Instruction;
 
 	private tutorialTexts = [`Welcome to the tutorial level!
 	Arrange the blocks by drag and drop, then click execute to control the crane.
@@ -83,9 +86,67 @@ export default class TutorialLevel extends Level {
 				tutorial.destroy();
 			}
 		});
+
+		this.makeInstructions();
 	}
 
     update(){
         super.update();
     }
+
+	makeInstructions() {
+		let currX = 100;
+        let currY = 50;
+		
+		const block_counts = [1, 1, 1, 2, 1, 1, 1, 1, 1];
+        const block_types = ["start", "up", "down", "left", "right", "grab", "release", "loop", "endloop"];
+
+        for(let i = 0; i < block_counts.length; i ++) {
+
+            for(let j = 0; j < block_counts[i]; j++) {
+                const fred = new Instruction(this, currX, currY, block_types[i] as InstructionType);
+                this.Instructions?.add([fred,fred.instructionText]);
+                currY += 50;
+                if(currY >= 400) {
+                    currX += 100;
+                    currY = 50;
+                }
+
+				//nasty if block that sets the class variables I know it's bad code i just dont really have time to do it right
+				if(block_types[i] === "start") {
+					this.start_instruction = fred;
+				}
+				else if (block_types[i] === "up") {
+					this.up_instruction = fred;
+				}
+				else if (block_types[i] === "down") {
+					this.down_instruction = fred;
+				}
+				else if (block_types[i] === "right") {
+					this.right_instruction = fred;
+				}
+				else if (block_types[i] === "left") {
+					if(j === 0)
+					this.left1_instruction = fred;
+					else
+					this.left2_instruction = fred;
+				}
+				else if (block_types[i] === "loop") {
+					this.loop_instruction = fred;
+				}
+				else if (block_types[i] === "endloop") {
+					this.endloop_instruction = fred;
+				}
+				else if (block_types[i] === "grab") {
+					this.grab_instruction = fred;
+				}
+				else if (block_types[i] === "release") {
+					this.release_instruction = fred;
+				}
+        }
+		}
+		const fred = new Instruction(this, currX, currY, "number", 2);
+		this.Instructions?.add([fred,fred.instructionText]);
+		this.number_instruction = fred;
+	}
 }
