7bd19cf755e8d71e86e1b8d456d7bffc1cf4ccfc
ydnamjs <ydnamjs@udel.edu>
2023-05-10 11:40:59 -0400

    Added additional level and made level start at 1 instead of 0

--- src/scenes/LevelSelectScene.ts
+++ src/scenes/LevelSelectScene.ts
@@ -6,7 +6,9 @@ export default class LevelSelectScene extends Phaser.Scene {
 	// private LevelBtns?: Array<Phaser.GameObjects>;
 
 	/* SCENE CONSTANTS */
-	private LEVELS: Array<GridData> = [{ width: 5, height: 5,
+	private LEVELS: Array<GridData> = [
+		//level 1
+		{ width: 5, height: 5,
 		gridObjects: [
 			["none", "none", "none", "none", "none"],
 			["none", "none", "none", "none", "none"],
@@ -33,7 +35,66 @@ export default class LevelSelectScene extends Phaser.Scene {
 		release_blocks: 2,
 		number_blocks: [],
 
-	}, { width: 5, height: 5,
+	}, 
+	//level 2
+	{ width: 5, height: 5,
+		gridObjects: [
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "none"],
+			["none", "none", "crane", "none", "none"],
+			["none", "none", "none", "none", "none"],
+			["crate-red", "crate-green", "crate-blue", "none", "none"]
+		],
+		gridObjectives: [
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "none"],
+			["none", "none", "crate-green", "none", "none"],
+			["none", "none", "crate-red", "none", "none"],
+			["none", "none", "crate-blue", "none", "none"]
+		],
+
+		start_blocks: 1,
+		loop_blocks: 0,
+		endloop_blocks: 0,
+		left_blocks: 3,
+		right_blocks: 3,
+		down_blocks: 2,
+		up_blocks: 3,
+		grab_blocks: 2,
+		release_blocks: 2,
+		number_blocks: [],
+
+	}, 
+	//level 3
+	{ width: 5, height: 5,
+		gridObjects: [
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "crane"],
+			["none", "none", "none", "none", "none"],
+			["crate-brown", "none", "none", "none", "crate-brown"]
+		],
+		gridObjectives: [
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "none"],
+			["none", "none", "none", "none", "crate-brown"],
+			["none", "none", "none", "none", "crate-brown"]
+		],
+
+		start_blocks: 1,
+		loop_blocks: 2,
+		endloop_blocks: 2,
+		left_blocks: 1,
+		right_blocks: 1,
+		down_blocks: 1,
+		up_blocks: 1,
+		grab_blocks: 1,
+		release_blocks: 1,
+		number_blocks: [4, 4],
+
+	}, 
+	{ width: 5, height: 5,
 		gridObjects: [
 			["none", "none", "crane", "none", "none"],
 			["none", "none", "none", "none", "none"],
@@ -65,9 +126,9 @@ export default class LevelSelectScene extends Phaser.Scene {
 	}
 
 	create() {
-		for (let i = 0; i < this.LEVELS.length; i++) {
+		for (let i = 1; i < this.LEVELS.length + 1; i++) {
 		// for (let i = 0; i < 20; i++) {
-			const levelButton = this.add.rectangle(100 + (i % 6) * 100, 100 + Math.floor(i / 6) * 100, 80, 80, 0x204060, 1);
+			const levelButton = this.add.rectangle((i % 6) * 100, 100+ Math.floor(i / 6) * 100, 80, 80, 0x204060, 1);
 			levelButton.setInteractive();
 			levelButton.on('pointerover', () => {
 				levelButton.setFillStyle(0x204060, 0.6);
@@ -75,7 +136,7 @@ export default class LevelSelectScene extends Phaser.Scene {
 			levelButton.on('pointerout', () => {
 				levelButton.setFillStyle(0x204060, 1);
 			});
-			const levelText = this.add.text(100 + (i % 6) * 100, 100 + (Math.floor(i / 6)) * 100, `${i}`, {
+			const levelText = this.add.text((i % 6) * 100, 100 + (Math.floor(i / 6)) * 100, `${i}`, {
 				fontSize: '40px',
 				color: '#fff',
 			});
